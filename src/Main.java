9c9941c68aff624814785c67523aa61df0fe59df
Marika <marikarumba@mail.ru>
2022-11-10 22:00:46 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Passport passport1 = new Passport(
+                1234,
+                "Ф",
+                "И",
+                null,
+                "17.04.1990");
+
+        Set<Passport> passports = new HashSet<>();
     }
 }
